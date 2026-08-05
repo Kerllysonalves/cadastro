11039faf7723c021177d01dbbd9484f331dadcf9
Kerllysonalves <kershguitar16@gmail.com>
2026-08-04 23:54:09 -0300

    Atualização

--- cadastro.py
+++ cadastro.py
@@ -20,4 +20,113 @@ def abrir_cadastro():
 )
     titulo.pack(pady=20)
 
-   
+    #nome
+    tk.Label(
+        cadastro,   
+        text="Nome completo",
+        bg="#f4f6f9",font=("Segoe UI", 11),
+    ).pack(anchor="w", padx=50)
+
+    entry_nome = tk.Entry(
+        cadastro,
+        font=("Segoe UI", 11),
+        width=40
+    )
+    entry_nome.pack(pady=5)
+
+    #Usuário
+    tk.Label(
+        cadastro,
+        text="Usuário",
+        bg="#f4f6f9",font=("Segoe UI", 11),
+    ).pack(anchor="w", padx=50)
+
+    entry_usuario = tk.Entry(
+        cadastro,
+        font=("Segoe UI", 11),
+        width=40
+    )
+    entry_usuario.pack(pady=5)
+
+    #Senha
+    tk.Label(
+        cadastro,
+        text="Senha",
+        bg="#f4f6f9",font=("Segoe UI", 11),
+    ).pack(anchor="w", padx=50)
+
+    entry_senha = tk.Entry(
+        cadastro,
+        font=("Segoe UI", 11),
+        width=40,
+        show="*"
+    )
+    entry_senha.pack(pady=5)
+
+#Confirmar senha
+    tk.Label(
+        cadastro,
+        text="Confirmar Senha",
+        bg="#f4f6f9",font=("Segoe UI", 11),
+    ).pack(anchor="w", padx=50)
+
+    entry_confirmar_senha = tk.Entry(
+        cadastro,
+        font=("Segoe UI", 11),
+        width=40,
+        show="*"
+    )
+    entry_confirmar_senha.pack(pady=5)
+
+#Cargo
+
+    tk.Label(
+        cadastro,
+        text="Cargo",
+        bg="#f4f6f9",font=("Segoe UI", 11),
+    ).pack(anchor="w", padx=50)
+
+    cargos = [
+        "Administrador",
+        "Pastor",
+        "Recepção",
+        "Mídia",
+        "Tesoureiro"
+    ]
+
+    combo_cargo = ttk.Combobox(
+        cadastro,
+        values=cargos,
+        width=37,
+        state="readdonly"
+    )
+
+    combo_cargo.current(0)
+    combo_cargo.pack(pady=5)
+
+#Botões
+
+    frame_botoes = tk.Frame(cadastro, bg="#f4f6f9")
+    frame_botoes.pack(pady=30)
+
+    btn_salvar = tk.Button(
+        frame_botoes,
+        text="Salvar",
+        width=15,
+        bg="#198754",
+        fg="white",
+        font=("Segoe UI", 10, "bold")
+)
+    btn_salvar.grid(row=0, column=0, padx=10)
+
+    btn_cancelar = tk.Button(
+        frame_botoes,
+        text="Cancelar",
+        width=15,
+        bg="#dc3545",
+        fg="white",
+        font=("Segoe UI", 10, "bold"),
+        command=cadastro.destroy
+)
+
+    btn_cancelar.grid(row=0, column=1, padx=10)
