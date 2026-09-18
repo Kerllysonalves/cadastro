871362c5ea11a516984703b05e62eb4aa9e0c145
Kerllysonalves <kershguitar16@gmail.com>
2026-09-18 10:09:47 -0300

    Segunda alteração

--- projetotest/test.py
+++ projetotest/test.py
@@ -1,7 +1,7 @@
 print("Olá, mundo!")
 
 produtos = []
-
+# FUNÇÃO PARA CADASTRAR PRODUTOS
 def cadastrar_produtos():
     print("---CADASTRO DE PRODUTOS---")
 
@@ -15,7 +15,7 @@ def cadastrar_produtos():
 
     produtos.append(produto)
     print("\nPRODUTO CADASTRADO COM SUCESSO!")
-
+# FUNÇÃO PARA LISTAR PRODUTOS
 def listar_produtos():
     print("\n---PRODUTOS CADASTRADOS---")
 
@@ -29,11 +29,30 @@ def listar_produtos():
         f"Valor do kg: R$ {produto['preco_kg']:.2f}"
     )
 
+# FUNÇÃO PARA ALTERAR PREÇOS
+def alterar_preco():
+    print("\n---ALTERAR PREÇO---")
+    nome_pesquisado = input("Pesquisar produto: ")
+
+    for produto in produtos:
+        if produto["nome"].lower() == nome_pesquisado.lower():
+
+            print(f"Produto encontrado: {produto['nome']}")
+            print(f"Preço atual: R$ {produto['preco_kg']:.2f}")
+
+            novo_preco =float(input("Digite o novo valor do kg: "))
+
+            produto["preco_kg"] = novo_preco
+
+            print("Preço alterado com sucesso!")
+            return
+
 while True:
     print("\n===AÇOUGUE===")
     print("1- Cadastrar produto")
     print("2- Listar produto")
-    print("3- Sair")
+    print("3- Alterar preço")
+    print("4- Sair")
 
     opcao = input("Escolha uma opção: ")
 
@@ -44,9 +63,12 @@ while True:
         listar_produtos()
 
     elif opcao == "3":
+        alterar_preco()
+
+    elif opcao == "4":
         print("Programa encerrado.")
         break
-
+    
     else:
         print("Opção inválida.")
     
